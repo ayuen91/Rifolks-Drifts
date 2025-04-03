77b9488a7bc76c86363a6344968ed54019a84797
ayuen <ayuenito@gmail.com>
2025-04-03 20:58:54 +0300

    update build and middleware manifests for improved performance and compatibility

--- frontend/.next/server/server-reference-manifest.js
+++ frontend/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"LAc93RZdc1nizV6aKZv4AyANbzQWCggDBH98KA4mVQI=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"\n}"
